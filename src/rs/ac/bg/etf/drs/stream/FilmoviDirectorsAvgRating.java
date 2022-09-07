421c71ac8d7a8b147f617a51950932d15c21a2af
sofijad996 <71739881+sofijad996@users.noreply.github.com>
2022-09-07 10:55:05 +0200

    Add files via upload

--- src/rs/ac/bg/etf/drs/stream/FilmoviDirectorsAvgRating.java
+++ src/rs/ac/bg/etf/drs/stream/FilmoviDirectorsAvgRating.java
@@ -1,77 +1,80 @@
 package rs.ac.bg.etf.drs.stream;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.Arrays;
-import java.util.Collection;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
+
 
 public class FilmoviDirectorsAvgRating {
 
 	public static void main(String[] args) throws IOException {
 		String filepathTitleCrew = "title_crew.tsv";
 		String filepathTitleRatings = "title_ratings.tsv";
+		String outputFileName = "outputStream.tsv";
+
+		Stream<String> titleCrewStream = Files.lines(Path.of(filepathTitleCrew)).parallel();
+		List<DirectorRating> titleDirectors = titleCrewStream
+				.skip(1)
+				.map(s -> new TitleCrew(s))
+				.flatMap(s -> s.getFilmDirectors().stream())
+				.map(s -> new DirectorRating(s))
+				.collect(Collectors.toList());
+		System.out.println("Done processing titleCrew");
+
+		Stream<String> titleRatingsStream = Files.lines(Path.of(filepathTitleRatings)).parallel();
+		titleRatingsStream
+				.skip(1)
+				.map(s -> new TitleRatings(s))
+				//.filter(s -> titleDirectors.stream().anyMatch(td -> td.tconst.equals(s.tconst)))
+				.forEach(s -> {
+					titleDirectors.stream()
+					.forEach(td -> {
+						if (td.getTitle().equals(s.getTitle())) {
+							td.setRating(s.getRating());
+						}
+					});
+				});
 		
-		List<String> titleCrew = new LinkedList<String>();
-		List<String> titleRatings = new LinkedList<String>();
-		Map<String, Double> directorsRating = new HashMap<String, Double>();
-		
-		try (Stream<String> titleCrewStream = Files.lines(Path.of(filepathTitleCrew)).parallel()) {
-			titleCrew =  titleCrewStream
-					.skip(1)
-					.map(s -> new TitleCrew(s))
-					.flatMap(s -> s.getFilmDirectors().stream())
-					.collect(Collectors.toList());
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		
-		try (Stream<String> titleRatingsStream = Files.lines(Path.of(filepathTitleRatings)).parallel()) {
-			titleRatings = titleRatingsStream
-					.skip(1)
-					.map(s -> new TitleRatings(s))
-					.flatMap(s -> s.getFilmRatings().stream())
-					.collect(Collectors.toList());
+		System.out.println("Done processing titleRatings");
 			
-		//	for (String item : titleRatings) System.out.println(item);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		
-		directorsRating = titleRatings
-				.parallelStream()
-				.forEach(tr -> {
-					titleCrew.stream()
-				     .filter(tc -> getFilmName(tc).equals(getFilmName(tr)))
-				     .map(tr -> tr + "-" + getDirectorName(tc))
-				     .get();
-				})				
-				.map(tr -> new DirectorRating(tr))
+		Map<String, Double> directorsRating = titleDirectors.stream()
 				.collect(Collectors.groupingByConcurrent(DirectorRating::getDirectorName, Collectors.averagingDouble(DirectorRating::getRating)));
+
+		System.out.println("Writing to file...");
 		
+		try {
+			BufferedWriter bf = new BufferedWriter(new FileWriter(outputFileName));
+
+	        for(Entry<String, Double> entry : directorsRating.entrySet()) {
+	        	bf.write(entry.getKey() + ": " + entry.getValue());
+	            bf.newLine();
+	        }	            
+	        bf.flush();	 
+	        bf.close();
+	        
+	    } catch (IOException e) {
+	        e.printStackTrace();
+	    } 
+		
+		System.out.println("Done writing to file!");		
+
 	}
-	
-	
-	static String getFilmName(String line) {
-		String result = line.substring(0, 9);
-		return result;
-	}
-	
-	static String getDirectorName(String line) {
-		String result = line.substring(9);
-		return result;
-	}
-	
+
 	public static class TitleCrew {
 		public String tconst;
 		String[] directors;
-		
+
 		public TitleCrew(String line) {
 			String[] elements = line.split("\t");
 			tconst = elements[0];
@@ -79,9 +82,9 @@ public class FilmoviDirectorsAvgRating {
 				directors = new String[0];
 			} else {
 				directors = elements[1].split(",");
-			}			
+			}
 		}
-		
+
 		public List<String> getFilmDirectors() {
 			List<String> result = new LinkedList<>();
 			for (String director : directors) {
@@ -91,43 +94,65 @@ public class FilmoviDirectorsAvgRating {
 			return result;
 		}
 	}
-	
+
 	public static class TitleRatings {
 		public String tconst;
 		public Double rating;
-		
+
 		public TitleRatings(String line) {
 			String[] elements = line.split("\t");
-			tconst = elements[0];
-			rating = Double.parseDouble(elements[1]);
-		}	
-		
+			this.tconst = elements[0];
+			this.rating = Double.parseDouble(elements[1]);
+		}
+
 		List<String> getFilmRatings() {
 			List<String> result = new LinkedList<>();
 			String filmRating = tconst + "-" + rating;
 			result.add(filmRating);
 			return result;
 		}
+
+		Double getRating() {
+			return rating;
+		}
+
+		String getTitle() {
+			return this.tconst;
+		}
+
 	}
-	
+
 	public static class DirectorRating {
 		public String tconst;
 		public Double rating;
 		public String director;
-		
+
 		public DirectorRating(String line) {
 			String[] elements = line.split("-");
-			tconst = elements[0];
-			rating = Double.parseDouble(elements[1]);
-			director = elements[2];
+			this.tconst = elements[0];
+			this.director = elements[1];
+			this.rating = 5.0;
 		}
-		
-		 String getDirectorName() {
+
+		String getDirectorName() {
 			return director;
 		}
-		
-		 Double getRating() {
-			 return rating;
-		 }
+
+		String getTitle() {
+			return this.tconst;
+		}
+
+		Double getRating() {
+			return rating;
+		}
+
+		void setRating(double rating) {
+			this.rating = rating;
+		}
+
+		@Override
+		public String toString() {
+			return tconst + " " + director + " " + rating;
+		}
 	}
 }
